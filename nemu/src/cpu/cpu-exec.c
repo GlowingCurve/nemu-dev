c812197a3ff7a3582c77d6a0f4204c7c06a11bb8
薛朝意 <25149100021@stu.xidian.edu.cn>
2026-08-24 22:17:06 +0800

    [Keywords]: optimization [Description]: optimize nemu by removing device_update; conduct experiments

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -88,7 +88,7 @@ static void execute(uint64_t n) {
     trace_and_difftest(&s, cpu.pc);
     if (nemu_state.state != NEMU_RUNNING)
       break;
-    IFDEF(CONFIG_DEVICE, device_update());
+    // IFDEF(CONFIG_DEVICE, device_update());
   }
 }
 
